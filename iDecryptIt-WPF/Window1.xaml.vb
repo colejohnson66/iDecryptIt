2c4d2abc7d52b2c510ed09a9d97f90833efeaeae
Cole Tobin <coleharrisjohnson@gmail.com>
2011-07-22 20:05:18 +0000

    transition to SVN standard complete, minus a few unneded files (like the deleted forms in obj/x86/Debug (i deleted bin and obj as they are created at buiild time, but unneded files arnt removed))

---
+++ iDecryptIt-WPF/Window1.xaml.vb
@@ -0,0 +1,83 @@
+﻿Imports System.IO
+Public Class Window1
+    ' Button values
+    Public topbutton As String = ""
+    Public bottombutton As String = ""
+    ' File paths
+    Public rundir As String = Directory.GetCurrentDirectory()
+    ' Update
+    Public major As String = "5"
+    Public updatemajor As String = ""
+    Public updatemajorurl As String = "http://theiphonewiki.com/wiki/index.php?title=User:Balloonhead66/Latest_stable_software_release/iDecryptIt/major&action=raw"
+    Public minor As String = "00"
+    Public updateminor As String = ""
+    Public updateminorurl As String = "http://theiphonewiki.com/wiki/index.php?title=User:Balloonhead66/Latest_stable_software_release/iDecryptIt/minor&action=raw"
+    Public rev As String = "3"
+    Public updaterev As String = ""
+    Public updaterevurl As String = "http://theiphonewiki.com/wiki/index.php?title=User:Balloonhead66/Latest_stable_software_release/iDecryptIt/revision&action=raw"
+    Public build As String = "1F38"
+    Public updatebuild As String = ""
+    Public updatebuildurl As String = "http://theiphonewiki.com/wiki/index.php?title=User:Balloonhead66/Latest_stable_software_release/iDecryptIt/build&action=raw"
+    Private Sub Window1_Loaded(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs) Handles Me.Loaded
+        Try
+            Dim clientCheck = New System.Net.WebClient()
+            ' Major
+            clientCheck.DownloadFile(updatemajorurl, rundir + "\major.txt")
+            Dim majorchecker As New System.IO.StreamReader(rundir + "\major.txt")
+            updatemajor = majorchecker.ReadToEnd
+            ' Minor
+            clientCheck.DownloadFile(updateminorurl, rundir + "\minor.txt")
+            Dim minorchecker As New System.IO.StreamReader(rundir + "\minor.txt")
+            updateminor = minorchecker.ReadToEnd
+            ' Revision
+            clientCheck.DownloadFile(updaterevurl, rundir + "\rev.txt")
+            Dim revchecker As New System.IO.StreamReader(rundir + "\rev.txt")
+            updaterev = revchecker.ReadToEnd
+            ' Build
+            clientCheck.DownloadFile(updatebuildurl, rundir + "\build.txt")
+            Dim buildchecker As New System.IO.StreamReader(rundir + "\build.txt")
+            updatebuild = buildchecker.ReadToEnd
+            ' Close lock handle on file
+            majorchecker.Close()
+            minorchecker.Close()
+            revchecker.Close()
+            buildchecker.Close()
+            Call compare()
+        Catch ex As Exception
+            MsgBox("Unable to contact The iPhone Wiki to download version info!", MsgBoxStyle.OkOnly, "Error!")
+            Me.Close()
+        End Try
+    End Sub
+    Public Sub compare()
+        Me.txtInstalled.Text = "Installed version: " + major + "." + minor + "." + rev + " (Build " + build + ")"
+        Me.txtAvailable.Text = "Latest version: " + updatemajor + "." + updateminor + "." + updaterev + " (Build " + updatebuild + ")"
+        If build = updatebuild Then
+            ' Titles
+            Me.Title = "No Update Available"
+            Me.TitleHeader.Text = "No Update Available"
+            ' Image
+            Me.ImageCheck.Visibility = Windows.Visibility.Hidden
+            Me.ImageNone.Visibility = Windows.Visibility.Visible
+            ' Button
+            Me.btnTop.Visibility = Windows.Visibility.Hidden
+            Me.btnBottom.Visibility = Windows.Visibility.Visible
+            bottombutton = "1"
+        Else
+            ' Titles
+            Me.Title = "Update Available"
+            Me.TitleHeader.Text = "Update Available"
+            ' Image
+            Me.ImageCheck.Visibility = Windows.Visibility.Hidden
+            Me.ImageAvailable.Visibility = Windows.Visibility.Visible
+            ' Button
+            Me.btnTop.Visibility = Windows.Visibility.Hidden
+            Me.btnBottom.Visibility = Windows.Visibility.Visible
+            bottombutton = "1"
+        End If
+    End Sub
+    Private Sub btnBottom_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles btnBottom.Click
+        If bottombutton = "1" Then
+            Me.Close()
+        End If
+    End Sub
+End Class
