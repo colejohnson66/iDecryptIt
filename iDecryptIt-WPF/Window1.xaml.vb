33e58e37c5957235d96b1c5b3539028547c88f0b
Cole Tobin <coleharrisjohnson@gmail.com>
2011-08-08 23:26:33 +0000

    begin the tags dir

--- iDecryptIt-WPF/Window1.xaml.vb
+++ iDecryptIt-WPF/Window1.xaml.vb
@@ -6,42 +6,16 @@ Public Class Window1
     ' File paths
     Public rundir As String = Directory.GetCurrentDirectory()
     ' Update
-    Public contacturl As String = "http://theiphonewiki.com/wiki/index.php?title=User:Balloonhead66/Latest_stable_software_release/iDecryptIt/"
-    Public major As String = "5"
-    Public updatemajor As String = ""
-    Public updatemajorurl As String = contacturl + "major&action=raw"
-    Public minor As String = "00"
-    Public updateminor As String = ""
-    Public updateminorurl As String = contacturl + "minor&action=raw"
-    Public rev As String = "4"
-    Public updaterev As String = ""
-    Public updaterevurl As String = contacturl + "revision&action=raw"
-    Public build As String = "1F75"
+    Public build As String = "1D02"
     Public updatebuild As String = ""
-    Public updatebuildurl As String = contacturl + "build&action=raw"
+    Public updatebuildurl As String = "http://theiphonewiki.com/wiki/index.php?title=User:Balloonhead66/Latest_stable_software_release/iDecryptIt/build&action=raw"
     Private Sub Window1_Loaded(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs) Handles Me.Loaded
         Try
             Dim clientCheck = New System.Net.WebClient()
-            ' Major
-            clientCheck.DownloadFile(updatemajorurl, rundir + "\major.txt")
-            Dim majorchecker As New System.IO.StreamReader(rundir + "\major.txt")
-            updatemajor = majorchecker.ReadToEnd
-            ' Minor
-            clientCheck.DownloadFile(updateminorurl, rundir + "\minor.txt")
-            Dim minorchecker As New System.IO.StreamReader(rundir + "\minor.txt")
-            updateminor = minorchecker.ReadToEnd
-            ' Revision
-            clientCheck.DownloadFile(updaterevurl, rundir + "\rev.txt")
-            Dim revchecker As New System.IO.StreamReader(rundir + "\rev.txt")
-            updaterev = revchecker.ReadToEnd
-            ' Build
             clientCheck.DownloadFile(updatebuildurl, rundir + "\build.txt")
             Dim buildchecker As New System.IO.StreamReader(rundir + "\build.txt")
             updatebuild = buildchecker.ReadToEnd
             ' Close lock handle on file
-            majorchecker.Close()
-            minorchecker.Close()
-            revchecker.Close()
             buildchecker.Close()
             Call compare()
         Catch ex As Exception
@@ -50,12 +24,13 @@ Public Class Window1
         End Try
     End Sub
     Public Sub compare()
-        Me.txtInstalled.Text = "Installed version: " + major + "." + minor + "." + rev + " (Build " + build + ")"
-        Me.txtAvailable.Text = "Latest version: " + updatemajor + "." + updateminor + "." + updaterev + " (Build " + updatebuild + ")"
+        Me.txtInstalled.Text = "Installed build: " + build
+        Me.txtAvailable.Text = "Latest build: " + updatebuild
         If build = updatebuild Then
             ' Titles
             Me.Title = "No Update Available"
-            Me.TitleHeader.Text = "No Update Available"
+            Me.TitleChecking.Visibility = Windows.Visibility.Hidden
+            Me.TitleNone.Visibility = Windows.Visibility.Visible
             ' Image
             Me.ImageCheck.Visibility = Windows.Visibility.Hidden
             Me.ImageNone.Visibility = Windows.Visibility.Visible
@@ -66,7 +41,8 @@ Public Class Window1
         Else
             ' Titles
             Me.Title = "Update Available"
-            Me.TitleHeader.Text = "Update Available"
+            Me.TitleChecking.Visibility = Windows.Visibility.Hidden
+            Me.TitleAvailable.Visibility = Windows.Visibility.Visible
             ' Image
             Me.ImageCheck.Visibility = Windows.Visibility.Hidden
             Me.ImageAvailable.Visibility = Windows.Visibility.Visible
