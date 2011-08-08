15b810874b041f05dac9a88d9d7bfe0a7020a50c
colejohnson66 <colejohnson66@209cdc03-6d9c-410e-8592-bf0908a4dd94>
2011-08-08 23:44:28 +0000

    the other 4 builds for the tags dir

---
+++ tags/iDecryptIt_5.00.4.1F75/iDecryptIt-WPF/SelectLangControl.xaml.vb
@@ -0,0 +1,19 @@
+﻿Public Class SelectLangControl
+    Private Sub btnOK_Click(sender As Object, e As System.Windows.RoutedEventArgs) Handles btnOK.Click
+        Dim selected As String = cmbSelect.SelectedIndex
+        If selected = "0" Then
+            Call enter("en")
+            MsgBox("You must restart iDecryptIt for changes to take effect.", MsgBoxStyle.Information)
+        ElseIf selected = "1" Then
+            Call enter("es")
+            MsgBox("Debe reiniciar iDecryptIt que los cambios surtan efecto.", MsgBoxStyle.Information)
+        End If
+        Me.Close()
+    End Sub
+    Public Sub enter(ByVal lang As String)
+        Dim langcode As Microsoft.Win32.RegistryKey
+        langcode = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("SOFTWARE\\Cole Stuff\\iDecryptIt")
+        langcode = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Cole Stuff\\iDecryptIt", True)
+        Microsoft.Win32.Registry.SetValue("HKEY_CURRENT_USER\SOFTWARE\\Cole Stuff\\iDecryptIt", "language", lang, Microsoft.Win32.RegistryValueKind.String)
+    End Sub
+End Class
---
+++ tags/iDecryptIt_5.00.4.1F75/iDecryptIt-WPF/SubmitKey.xaml.vb
@@ -0,0 +1,6 @@
+﻿Imports System.IO
+Public Class SubmitKey
+    Private Sub SubmitKey_Loaded(sender As Object, e As System.Windows.RoutedEventArgs) Handles Me.Loaded
+        Me.web.Navigate(New Uri(Directory.GetCurrentDirectory() + "\help\submitkey.html"))
+    End Sub
+End Class
