04cb96806e9b4fbc84d554ec7a8041cc2dd85a37
Cole Tobin <coleharrisjohnson@gmail.com>
2011-10-14 14:34:53 +0000

    images modified in help dir. no more web tab

--- iDecryptIt-WPF/SubmitKey.xaml.vb
+++
@@ -1,6 +0,0 @@
-﻿Imports System.IO
-Public Class SubmitKey
-    Private Sub SubmitKey_Loaded(sender As Object, e As System.Windows.RoutedEventArgs) Handles Me.Loaded
-        Me.web.Navigate(New Uri(Directory.GetCurrentDirectory() + "\help\submitkey.html"))
-    End Sub
-End Class
