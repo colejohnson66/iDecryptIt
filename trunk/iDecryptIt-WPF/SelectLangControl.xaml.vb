6681e9d19699b919d0ecc7b7b0c7ee731caaba4d
colejohnson66 <colejohnson66@209cdc03-6d9c-410e-8592-bf0908a4dd94>
2011-10-25 03:05:22 +0000

    marquee on select lang control (with gradient for fade), dmg file names for iphone 2g on 1.x final, key + row for 9a336a (5.0/4.4.2) on atv

--- trunk/iDecryptIt-WPF/SelectLangControl.xaml.vb
+++ trunk/iDecryptIt-WPF/SelectLangControl.xaml.vb
@@ -1,19 +1,48 @@
-﻿Public Class SelectLangControl
-    Private Sub btnOK_Click(sender As Object, e As System.Windows.RoutedEventArgs) Handles btnOK.Click
-        Dim selected As String = cmbSelect.SelectedIndex
-        If selected = "0" Then
-            Call enter("en")
+﻿Imports Microsoft.Win32
+Public Class SelectLangControl
+    Private Sub btnOK_Click() Handles btnOK.Click
+        Dim home As Window = New MainWindow
+        Dim selected As Integer = cmbSelect.SelectedIndex
+        If selected = 0 Then
+            Call enter("eng")
             MsgBox("You must restart iDecryptIt for changes to take effect.", MsgBoxStyle.Information)
-        ElseIf selected = "1" Then
-            Call enter("es")
+        ElseIf selected = 1 Then
+            Call enter("spa")
             MsgBox("Debe reiniciar iDecryptIt que los cambios surtan efecto.", MsgBoxStyle.Information)
+        ElseIf selected = 2 Then
+            Call enter("hin")
+            MsgBox("", MsgBoxStyle.Information)
         End If
         Me.Close()
     End Sub
+    Private Sub SelectLangControl_Loaded() Handles Me.Loaded
+        Dim lang As String
+        Dim langcode As RegistryKey
+        langcode = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Cole Stuff\\iDecryptIt", True)
+        lang = Registry.GetValue("HKEY_CURRENT_USER\SOFTWARE\\Cole Stuff\\iDecryptIt", "language", "eng")
+        ' Failsafe for transition
+        If lang = "en" Then
+            lang = "eng"
+        ElseIf lang = "es" Then
+            lang = "spa"
+        End If
+        ' Change language
+        If lang = "eng" Then
+            cmbSelect.SelectedIndex = 0
+        ElseIf lang = "spa" Then
+            cmbSelect.SelectedIndex = 1
+        ElseIf lang = "hin" Then
+            cmbSelect.SelectedIndex = 2
+        Else
+            ' Fall back to English if is not any of the above
+            Registry.CurrentUser.DeleteSubKey("SOFTWARE\\Cole Stuff\\iDecryptIt")
+            cmbSelect.SelectedIndex = 0
+        End If
+    End Sub
     Public Sub enter(ByVal lang As String)
-        Dim langcode As Microsoft.Win32.RegistryKey
-        langcode = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("SOFTWARE\\Cole Stuff\\iDecryptIt")
-        langcode = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Cole Stuff\\iDecryptIt", True)
-        Microsoft.Win32.Registry.SetValue("HKEY_CURRENT_USER\SOFTWARE\\Cole Stuff\\iDecryptIt", "language", lang, Microsoft.Win32.RegistryValueKind.String)
+        Dim langcode As RegistryKey
+        langcode = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Cole Stuff\\iDecryptIt")
+        langcode = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Cole Stuff\\iDecryptIt", True)
+        Registry.SetValue("HKEY_CURRENT_USER\SOFTWARE\\Cole Stuff\\iDecryptIt", "language", lang, RegistryValueKind.String)
     End Sub
 End Class
