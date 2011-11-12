abf48985286c9cfa7e43a1e81e24e20785d4efd0
Cole Tobin <coleharrisjohnson@gmail.com>
2011-11-12 06:18:38 +0000

    branch 1 merge to head

--- iDecryptIt-WPF/Application.xaml.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Application
-
-    'Application-level events, such as Startup, Exit, and DispatcherUnhandledException
-    'can be handled in this file.
-
-    Shared Function DoEvents() As Integer
-        Throw New NotImplementedException
-    End Function
-End Class
---
+++ iDecryptIt-WPF/SelectLangControl.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace iDecryptIt_WPF
+{
+    /// <summary>
+    /// Interaction logic for SelectLangControl.xaml
+    /// </summary>
+    public partial class SelectLangControl : Window
+    {
+        public SelectLangControl()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string lang;
+            RegistryKey langcode;
+            langcode = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Cole Stuff\\iDecryptIt", true);
+            lang = (string)langcode.GetValue("language");
+            
+            // Failsafe for transition
+            if (lang == "en")
+            {
+                lang = "eng";
+            }
+            if (lang == "es")
+            {
+                lang = "spa";
+            }
+
+            // Change language
+            if (lang == "eng")
+            {
+                cmbSelect.SelectedIndex = 0;
+            }
+            else if (lang == "spa")
+            {
+                cmbSelect.SelectedIndex = 1;
+            }
+            else if (lang == "hin")
+            {
+                cmbSelect.SelectedIndex = 2;
+            }
+            else
+            {
+                // Fall back to English if is not any of the above
+                Registry.CurrentUser.DeleteSubKey("SOFTWARE\\Cole Stuff\\iDecryptIt");
+                cmbSelect.SelectedIndex = 0;
+            }
+        }
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            Window self = new SelectLangControl();
+            int selected = cmbSelect.SelectedIndex;
+            if (selected == 0)
+            {
+                enter("eng");
+                MessageBox.Show("You must restart iDecryptIt for changes to take effect", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            if (selected == 1)
+            {
+                enter("spa");
+                MessageBox.Show("Debe reiniciar iDecryptIt que los cambios surtan efecto", "Información", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            if (selected == 2)
+            {
+                enter("hin");
+                MessageBox.Show("", "", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+        private void enter(string lang)
+        {
+            RegistryKey langcode;
+            langcode = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Cole Stuff\\iDecryptIt");
+            langcode = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Cole Stuff\\iDecryptIt", true);
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Cole Stuff\\iDecryptIt", "language", lang, RegistryValueKind.String);
+            Close();
+        }
+    }
+}
