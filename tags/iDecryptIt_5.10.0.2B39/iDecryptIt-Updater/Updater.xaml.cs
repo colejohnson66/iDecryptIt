7acd5d60370c0db177a555690aeabd14dc125ba3
colejohnson66 <colejohnson66@209cdc03-6d9c-410e-8592-bf0908a4dd94>
2011-12-19 18:16:07 +0000

    tags for 5.10.0

---
+++ tags/iDecryptIt_5.10.0.2B39/iDecryptIt-Updater/Updater.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace iDecryptIt_Updater
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string tempdir = System.IO.Path.GetTempPath() + "\\Cole Stuff\\iDecryptIt\\";
+        string rundir = Directory.GetCurrentDirectory() + "\\";
+        string contacturl = "http://theiphonewiki.com/wiki/index.php?title=User:Balloonhead66/Latest_stable_software_release/iDecryptIt&action=raw";
+        string checker;
+        string major = "5";
+        string minor = "10";
+        string rev = "0";
+        string build = "2B39";
+        string updatemajor;
+        string updateminor;
+        string updaterev;
+        string updatebuild;
+
+        Run run = new Run();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, EventArgs e)
+        {
+            // Because the updater downloads to .exe.new after extraction,
+            // if that file exists, delete the .exe and rename the .exe.new to .exe
+            if (File.Exists(rundir + "iDecryptIt.exe.new"))
+            {
+                // Kill iDecryptIt
+                /*Process[] runningapps = Process.GetProcesses();
+                foreach (Process p in runningapps)
+                {
+                    if (p.ProcessName == "iDecryptIt" || p.ProcessName == "iDecryptIt.vshost")
+                    {
+                        p.Kill();
+                        while (!p.HasExited)
+                        {
+                        }
+                    }
+                }
+                try
+                {
+                    File.Delete(rundir + "iDecryptIt.exe");
+                    while (File.Exists(rundir + "iDecryptIt.exe"))
+                    {
+                    }
+                    File.Move(rundir + "iDecryptIt.exe.new", rundir + "iDecryptIt.exe");
+
+                    // Relaunch iDecryptIt
+                    run.DoCMD("iDecryptIt.exe", false);
+                    Close();
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    MessageBox.Show(
+                        "Unable to delete iDecryptIt!\n\n" +
+                        "Delete \"iDecryptIt.exe\" and move\n\"iDecryptIt.exe.new\" to \"iDecryptIt.exe\"\n\n" +
+                        "Exception: " + ex.Message,
+                        "iDecryptIt Updater",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                    Close();
+                }*/
+            }
+
+            btnTop.Visibility = Visibility.Hidden;
+
+            // Download the raw code
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(contacturl, @tempdir + "update.txt");
+                checker = File.ReadAllText(tempdir + "update.txt");
+            }
+            catch
+            {
+                MessageBox.Show("Unable to contact the iPhone Wiki to download version info!", "ERROR!", MessageBoxButton.OK);
+                Close();
+            }
+            string[] checkerArr = checker.Split('.');
+            updatemajor = checkerArr[0];
+            updateminor = checkerArr[1];
+            updaterev = checkerArr[2];
+            updatebuild = checkerArr[3];
+            
+            // Compare
+            if (build == updatebuild)
+            {
+                Close();
+            }
+            this.Title = "Update Available";
+            this.txtHeader.Text = "Update Available";
+            this.txtInstalled.Text = "Installed version: " + major + "." + minor + "." + rev + " (Build " + build + ")";
+            this.txtAvailable.Text = "Latest version: " + updatemajor + "." + updateminor + "." + updaterev + " (Build " + updatebuild + ")";
+        }
+        private void btnBottom_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void btnTop_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ tags/iDecryptIt_5.10.0.2B39/iDecryptIt-WPF/SplashScreen.xaml.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Timers;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace iDecryptIt_WPF
+{
+    /// <summary>
+    /// Interaction logic for SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        //Timer timer = new Timer();
+        //bool timer_GoneOff = false;
+
+        public SplashScreen()
+        {
+            //timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+            //timer.Interval = 1500;
+
+            // Grab .NET version
+            RegistryKey installed_versions = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP");
+            string[] version_names = installed_versions.GetSubKeyNames();
+            double Framework = Convert.ToDouble(version_names[version_names.Length - 1].Remove(0, 1), CultureInfo.InvariantCulture);
+            //int SP = Convert.ToInt32(installed_versions.OpenSubKey(version_names[version_names.Length - 1]).GetValue("SP", 0));
+
+            InitializeComponent();
+
+            // It can't close proporly if it has not been initialized
+            if (Framework < 4.0)
+            {
+                MessageBox.Show("You need .NET 4.0 or better to run iDecryptIt\n\nYou can download it at www.microsoft.com/net\nor through Windows Update", "iDecryptIt", MessageBoxButton.OK, MessageBoxImage.Warning);
+                this.Close(); // No need for dispatcher
+            }
+        }
+        /*private void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            timer_GoneOff = true;
+        }*/
+        private void updateprog(string text)
+        {
+            Action act = () =>
+            {
+                _updateprog(text);
+            };
+            Dispatcher.BeginInvoke(act);
+        }
+        private void _updateprog(string text)
+        {
+            lblProg.Content = text;
+        }
+        private void loadmain()
+        {
+            Action act = () =>
+            {
+                _loadmain();
+            };
+            Dispatcher.BeginInvoke(act);
+        }
+        private void _loadmain()
+        {
+            MainWindow mainwindow = new MainWindow();
+            mainwindow.Show();
+        }
+        private void close()
+        {
+            // Wait for 1.5 seconds to occur before closing
+            /*while (!timer_GoneOff)
+            {
+            }*/
+
+            Action act = () =>
+            {
+                _close();
+            };
+            Dispatcher.BeginInvoke(act);
+        }
+        private void _close()
+        {
+            this.Close();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Action act;
+            //timer.Enabled = true;
+
+            // Text
+            act = () =>
+            {
+                updateprog("Loading iDecryptIt");
+            };
+            Dispatcher.BeginInvoke(act);
+            
+            // Load
+            act = () =>
+            {
+                loadmain();
+            };
+            Dispatcher.BeginInvoke(act);
+
+            // Close
+            act = () =>
+            {
+                close();
+            };
+            Dispatcher.BeginInvoke(act);
+        }
+    }
+}
