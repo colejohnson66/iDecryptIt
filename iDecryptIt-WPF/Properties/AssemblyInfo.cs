d20783362a253c588c867cf1c449d099b25d08df
Cole Tobin <coleharrisjohnson@gmail.com>
2013-09-20 00:52:32 +0000

    fixed missing 7zip image

--- iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Resources;
 [assembly: ComVisible(true)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("7.0.0.1208")]
-[assembly: AssemblyFileVersion("7.0.0.1208")]
+[assembly: AssemblyVersion("7.0.0.1210")]
+[assembly: AssemblyFileVersion("7.0.0.1210")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
