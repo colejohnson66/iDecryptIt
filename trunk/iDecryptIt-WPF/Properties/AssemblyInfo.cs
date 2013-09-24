04777f61fbf20803255b9b28bb652bd33d5afd98
colejohnson66 <colejohnson66@209cdc03-6d9c-410e-8592-bf0908a4dd94>
2013-09-24 22:49:22 +0000

    added new keys

--- trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -1,11 +1,11 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Resources;
 
 [assembly: AssemblyTitle("iDecryptIt")]
 [assembly: AssemblyDescription("iOS firmware tools")]
-[assembly: AssemblyConfiguration("Beta")]
+[assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCompany("Hexware")]
 [assembly: AssemblyProduct("iDecryptIt")]
 [assembly: AssemblyCopyright("Copyright (C) 2010-2013")]
@@ -15,6 +15,6 @@ using System.Resources;
 [assembly: ComVisible(true)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("7.01.0.1225")]
-[assembly: AssemblyFileVersion("7.01.0.1225")]
+[assembly: AssemblyVersion("7.01.0.1226")]
+[assembly: AssemblyFileVersion("7.01.0.1226")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
