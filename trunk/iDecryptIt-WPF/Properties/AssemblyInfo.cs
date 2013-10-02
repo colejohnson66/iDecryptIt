1da6373851c0d88056f12ab3e7c191554129a730
colejohnson66 <colejohnson66@209cdc03-6d9c-410e-8592-bf0908a4dd94>
2013-10-02 04:31:50 +0000

    FINISHED! Nearly two YEARS in the making! The all new clean and polished iDecryptIt 7.02.0 (Build 2C60)
    Console version unreacheable
    Debug messages accessible with /debug switch

--- trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -1,3 +1,23 @@
+/* =============================================================================
+ * File:   AssemblyInfo.cs
+ * Author: Hexware
+ * =============================================================================
+ * Copyright (c) 2011-2013, Hexware
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * =============================================================================
+ */
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -15,6 +35,6 @@ using System.Resources;
 [assembly: ComVisible(true)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("7.01.0.1256")]
-[assembly: AssemblyFileVersion("7.01.0.1256")]
+[assembly: AssemblyVersion("7.02.0.1260")]
+[assembly: AssemblyFileVersion("7.02.0.1260")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
