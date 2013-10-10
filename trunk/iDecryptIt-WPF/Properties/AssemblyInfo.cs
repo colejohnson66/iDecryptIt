4c93544c4d6bb117d985be1a262d229f9c8eff2f
colejohnson66 <colejohnson66@209cdc03-6d9c-410e-8592-bf0908a4dd94>
2013-10-10 22:36:43 +0000

    updating version to 7.02.1

--- trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Resources;
 [assembly: ComVisible(true)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("7.02.0.1260")]
-[assembly: AssemblyFileVersion("7.02.0.1260")]
+[assembly: AssemblyVersion("7.02.1.1263")]
+[assembly: AssemblyFileVersion("7.02.1.1263")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
