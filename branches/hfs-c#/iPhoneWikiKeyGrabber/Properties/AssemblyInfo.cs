d31f16190cdb96ed53221d4e8dfb4b48249faa8a
colejohnson66 <colejohnson66@209cdc03-6d9c-410e-8592-bf0908a4dd94>
2014-01-20 02:46:23 +0000

    l';

---
+++ branches/hfs-c#/iPhoneWikiKeyGrabber/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+[assembly: AssemblyTitle("iPhoneWikiKeyGrabber")]
+[assembly: AssemblyDescription("Grab decryption keys from The iPhone Wiki")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hexware")]
+[assembly: AssemblyProduct("iPhoneWikiKeyGrabber")]
+[assembly: AssemblyCopyright("Copyright © 2012-2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("25e3f6af-4b61-4250-aaac-2f894698acbf")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
