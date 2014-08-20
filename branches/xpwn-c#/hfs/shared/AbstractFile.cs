afa73f6cb2afbfdc3b72578bef58b0b43155af6f
colejohnson66 <colejohnson66@209cdc03-6d9c-410e-8592-bf0908a4dd94>
2014-08-20 04:06:14 +0000

    hfs/includes/common.h -> shared/Common.cs

---
+++ branches/xpwn-c#/hfs/shared/AbstractFile.cs
@@ -0,0 +1,110 @@
+﻿/* =============================================================================
+ * File:   AbstractFile.cs
+ * Author: Cole Johnson
+ * =============================================================================
+ * Copyright (c) 2014, Cole Johnson
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * =============================================================================
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xpwn
+{
+    class AbstractFile
+    {
+
+    }
+}
+/*
+#ifndef ABSTRACTFILE_H
+#define ABSTRACTFILE_H
+
+#include "common.h"
+#include <stdint.h>
+
+typedef struct AbstractFile AbstractFile;
+typedef struct AbstractFile2 AbstractFile2;
+
+typedef size_t (*WriteFunc)(AbstractFile* file, const void* data, size_t len);
+typedef size_t (*ReadFunc)(AbstractFile* file, void* data, size_t len);
+typedef int (*SeekFunc)(AbstractFile* file, off_t offset);
+typedef off_t (*TellFunc)(AbstractFile* file);
+typedef void (*CloseFunc)(AbstractFile* file);
+typedef off_t (*GetLengthFunc)(AbstractFile* file);
+typedef void (*SetKeyFunc)(AbstractFile2* file, const unsigned int* key, const unsigned int* iv);
+
+typedef enum AbstractFileType {
+	AbstractFileTypeFile,
+	AbstractFileType8900,
+	AbstractFileTypeImg2,
+	AbstractFileTypeImg3,
+	AbstractFileTypeLZSS,
+	AbstractFileTypeIBootIM,
+	AbstractFileTypeMem,
+	AbstractFileTypeMemFile,
+	AbstractFileTypeDummy
+} AbstractFileType;
+
+struct AbstractFile {
+	void* data;
+	WriteFunc write;
+	ReadFunc read;
+	SeekFunc seek;
+	TellFunc tell;
+	GetLengthFunc getLength;
+	CloseFunc close;
+	AbstractFileType type;
+};
+
+struct AbstractFile2 {
+	AbstractFile super;
+	SetKeyFunc setKey;
+};
+
+
+typedef struct {
+	size_t offset;
+	void** buffer;
+	size_t bufferSize;
+} MemWrapperInfo;
+
+typedef struct {
+	size_t offset;
+	void** buffer;
+	size_t* bufferSize;
+	size_t actualBufferSize;
+} MemFileWrapperInfo;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	AbstractFile* createAbstractFileFromFile(FILE* file);
+	AbstractFile* createAbstractFileFromDummy();
+	AbstractFile* createAbstractFileFromMemory(void** buffer, size_t size);
+	AbstractFile* createAbstractFileFromMemoryFile(void** buffer, size_t* size);
+	AbstractFile* createAbstractFileFromMemoryFileBuffer(void** buffer, size_t* size, size_t actualBufferSize);
+	void abstractFilePrint(AbstractFile* file, const char* format, ...);
+	io_func* IOFuncFromAbstractFile(AbstractFile* file);
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+*/
---
+++ branches/xpwn-c#/hfs/shared/Common.cs
@@ -0,0 +1,91 @@
+﻿/* =============================================================================
+ * File:   Common.cs
+ * Author: Cole Johnson
+ * =============================================================================
+ * Copyright (c) 2014, Cole Johnson
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * =============================================================================
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xpwn
+{
+    public class Common
+    {
+        public const long TimeOffsetFromUnix = 2082844800;
+        public static bool IsLittleEndian = BitConverter.IsLittleEndian;
+
+        public static long AppleToUnixTime(long time)
+        {
+            return time - TimeOffsetFromUnix;
+        }
+
+        public static long UnixToAppleTime(long time)
+        {
+            return time + TimeOffsetFromUnix;
+        }
+
+        public static void FlipEndianBig(byte[] array)
+        {
+            if (IsLittleEndian)
+               Array.Reverse(array);
+        }
+
+        public static void FlipEndianLittle(byte[] array)
+        {
+            if (!IsLittleEndian)
+                Array.Reverse(array);
+        }
+
+        public static byte[] HexStringToByteArray(string hexString)
+        {
+            int length = hexString.Length / 2;
+            byte[] bytes = new byte[length];
+
+            for (int i = 0; i < length; i++)
+                bytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+
+            return bytes;
+        }
+
+        public static int[] HexStringToIntArray(string hexString)
+        {
+            int length = hexString.Length / 2;
+            int[] ints = new int[length];
+
+            for (int i = 0; i < length; i++)
+                ints[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+
+            return ints;
+        }
+
+        //unsigned char* decodeBase64(char* toDecode, size_t* dataLength);
+        //void writeBase64(struct AbstractFile* file, unsigned char* data, size_t dataLength, int tabLength, int width);
+        //char* convertBase64(unsigned char* data, size_t dataLength, int tabLength, int width);
+    }
+
+    public struct IOFunctionStruct
+    {
+        public byte[] Data;
+        public delegate int Read(int offset, int size, byte[] buffer);
+        public delegate int Write(int offset, int size, byte[] buffer);
+        public delegate void Close();
+    }
+}
