b4a678714c01afb195853a135c246be965b71416
Cole Tobin <coleharrisjohnson@gmail.com>
2014-09-01 01:16:37 +0000

    i don't know

--- iDecryptIt-WPF/MainWindow.xaml.cs
+++ iDecryptIt-WPF/MainWindow.xaml.cs
@@ -2,7 +2,7 @@
  * File:   MainWindow.xaml.cs
  * Author: Hexware
  * =============================================================================
- * Copyright (c) 2011-2014, Hexware
+ * Copyright (c) 2010-2014, Hexware
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
