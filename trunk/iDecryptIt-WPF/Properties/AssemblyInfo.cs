5545f4641a65eaaee32390e77b665a0c989db1d6
colejohnson66 <colejohnson66@209cdc03-6d9c-410e-8592-bf0908a4dd94>
2014-09-28 01:35:00 +0000

    Updated changelog.html Minor layout fix (doesn't change rendered content)

--- trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(true)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("8.00.0.1458")]
-[assembly: AssemblyFileVersion("8.00.0.1458")]
+[assembly: AssemblyVersion("8.00.0.1459")]
+[assembly: AssemblyFileVersion("8.00.0.1459")]
 [assembly: NeutralResourcesLanguage("en-US")]
