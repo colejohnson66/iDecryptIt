f0f276c708376aaefaabbd3386d4f503add0aa37
Cole Tobin <coleharrisjohnson@gmail.com>
2014-10-23 01:28:42 +0000

    Working on a new layout (nothing visual, simply to make code easier to maintain)

--- iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(true)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("8.10.0.1473")]
-[assembly: AssemblyFileVersion("8.10.0.1473")]
+[assembly: AssemblyVersion("8.10.0.1475")]
+[assembly: AssemblyFileVersion("8.10.0.1475")]
 [assembly: NeutralResourcesLanguage("en-US")]
