772e8529bf39a29d79a44b99c5ecaedc6d0dbbd1
colejohnson66 <colejohnson66@209cdc03-6d9c-410e-8592-bf0908a4dd94>
2014-11-03 00:07:06 +0000

    iPhone 5 (Global) -> iPhone 5 (CDMA)

--- trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(true)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("8.10.0.1477")]
-[assembly: AssemblyFileVersion("8.10.0.1477")]
+[assembly: AssemblyVersion("8.10.0.1478")]
+[assembly: AssemblyFileVersion("8.10.0.1478")]
 [assembly: NeutralResourcesLanguage("en-US")]
