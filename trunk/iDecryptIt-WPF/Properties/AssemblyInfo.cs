0cdbd6a8c3c156e61278db0c440d1ee16251ec65
colejohnson66 <colejohnson66@209cdc03-6d9c-410e-8592-bf0908a4dd94>
2014-11-17 03:36:52 +0000

    read.html -> readme.html

--- trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(true)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("8.10.0.1480")]
-[assembly: AssemblyFileVersion("8.10.0.1480")]
+[assembly: AssemblyVersion("8.10.0.1481")]
+[assembly: AssemblyFileVersion("8.10.0.1481")]
 [assembly: NeutralResourcesLanguage("en-US")]
