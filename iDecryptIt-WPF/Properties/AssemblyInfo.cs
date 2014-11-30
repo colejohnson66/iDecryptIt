eed67f9f59f909d0a57a8f656341dc9df77ce089
Cole Tobin <coleharrisjohnson@gmail.com>
2014-11-30 04:54:55 +0000

    Updated license information in help/readme.html Removed Licenses directory

--- iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(true)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("8.11.0.1498")]
-[assembly: AssemblyFileVersion("8.11.0.1498")]
+[assembly: AssemblyVersion("8.11.0.1499")]
+[assembly: AssemblyFileVersion("8.11.0.1499")]
 [assembly: NeutralResourcesLanguage("en-US")]
