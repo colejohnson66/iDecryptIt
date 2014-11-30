894b5cb9c5bcf85a8908375d78b180f4d65b0a10
colejohnson66 <colejohnson66@209cdc03-6d9c-410e-8592-bf0908a4dd94>
2014-11-30 06:08:34 +0000

    Visual Studio seems to want the app icon to be in the project root, not a subdirectory, so
    Removed Images/icon.ico (duplicate of icon.ico)

--- trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(true)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("8.11.0.1504")]
-[assembly: AssemblyFileVersion("8.11.0.1504")]
+[assembly: AssemblyVersion("8.11.0.1505")]
+[assembly: AssemblyFileVersion("8.11.0.1505")]
 [assembly: NeutralResourcesLanguage("en-US")]
