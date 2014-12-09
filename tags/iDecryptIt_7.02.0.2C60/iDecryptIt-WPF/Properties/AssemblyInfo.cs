338ff1967d6164b5e80351648514f05c81821ac2
colejohnson66 <colejohnson66@209cdc03-6d9c-410e-8592-bf0908a4dd94>
2014-12-09 04:52:44 +0000

    huh guess I never tagged 7.02.0...

---
+++ tags/iDecryptIt_7.02.0.2C60/iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+/* =============================================================================
+ * File:   AssemblyInfo.cs
+ * Author: Hexware
+ * =============================================================================
+ * Copyright (c) 2011-2013, Hexware
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * =============================================================================
+ */
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+[assembly: AssemblyTitle("iDecryptIt")]
+[assembly: AssemblyDescription("iOS firmware tools")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("Hexware")]
+[assembly: AssemblyProduct("iDecryptIt")]
+[assembly: AssemblyCopyright("Copyright (C) 2010-2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(true)]
+[assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
+
+[assembly: AssemblyVersion("7.02.0.1260")]
+[assembly: AssemblyFileVersion("7.02.0.1260")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
