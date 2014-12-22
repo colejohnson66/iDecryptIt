587909a1f0d2b832370bb8628db06a84e095c252
Cole Tobin <coleharrisjohnson@gmail.com>
2014-12-22 02:22:56 +0000

    Removed keys of incorrect length Corrected iPad2,2 11D257's iBoot key "SEPFirmware" -> "SEP-Firmware" in MainWindow.xaml

--- iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(true)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("8.11.0.1508")]
-[assembly: AssemblyFileVersion("8.11.0.1508")]
+[assembly: AssemblyVersion("8.11.0.1511")]
+[assembly: AssemblyFileVersion("8.11.0.1511")]
 [assembly: NeutralResourcesLanguage("en-US")]
