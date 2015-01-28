d425a29361dd5330ca967abe91f9078740313c42
Cole Tobin <coleharrisjohnson@gmail.com>
2015-01-28 01:41:09 +0000

    adding 8.1.3 support

--- iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(true)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("8.13.0.1522")]
-[assembly: AssemblyFileVersion("8.13.0.1522")]
+[assembly: AssemblyVersion("8.13.0.1523")]
+[assembly: AssemblyFileVersion("8.13.0.1523")]
 [assembly: NeutralResourcesLanguage("en-US")]
