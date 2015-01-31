8d1ab0e531c500beb9703f058b70fa61032f9cf5
Cole Tobin <coleharrisjohnson@gmail.com>
2015-01-31 01:40:44 +0000

    it's 7.0.3

--- iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -41,5 +41,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("8.13.0.1527")]
-[assembly: AssemblyFileVersion("8.13.0.1527")]
+[assembly: AssemblyVersion("8.13.0.1528")]
+[assembly: AssemblyFileVersion("8.13.0.1528")]
