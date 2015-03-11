6d323e174c6c9637948b798cf6fdf259b91e4eb9
colejohnson66 <colejohnson66@209cdc03-6d9c-410e-8592-bf0908a4dd94>
2015-03-11 04:37:50 +0000

    Version bump to 8.20.0 Added support for 8.2 and ATV 7.1 (8.2)

--- trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("8.13.1.1564")]
-[assembly: AssemblyFileVersion("8.13.1.1564")]
+[assembly: AssemblyVersion("8.20.0.1567")]
+[assembly: AssemblyFileVersion("8.20.0.1567")]
 [assembly: NeutralResourcesLanguage("en-us")]
