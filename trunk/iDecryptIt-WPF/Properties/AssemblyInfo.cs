893797e9a3346c9565980bfac92ca30cf2e23d49
colejohnson66 <colejohnson66@209cdc03-6d9c-410e-8592-bf0908a4dd94>
2015-09-26 06:46:42 +0000

    Version bump to 9.01.0 Added 9.0.1 support Added AppleTV5,3 (Apple TV 4G) to GlobalVars.DeviceNames (won't show up in MainWindow until a /public/ firmware is released)

--- trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("9.00.0.1583")]
-[assembly: AssemblyFileVersion("9.00.0.1583")]
+[assembly: AssemblyVersion("9.01.0.1586")]
+[assembly: AssemblyFileVersion("9.01.0.1586")]
 [assembly: NeutralResourcesLanguage("en-us")]
