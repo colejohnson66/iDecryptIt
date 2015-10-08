c42cefff0befe69c38b49e15b05171d49f275eaa
colejohnson66 <colejohnson66@209cdc03-6d9c-410e-8592-bf0908a4dd94>
2015-10-08 23:12:20 +0000

    * Added support for `BatteryCharging' (used in IMG2 firmwares) * Fixed support for GM Root FS (key file format will change as GM Root FS has a different filename than the regular Root FS)

--- trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("9.01.0.1587")]
-[assembly: AssemblyFileVersion("9.01.0.1587")]
+[assembly: AssemblyVersion("9.01.0.1590")]
+[assembly: AssemblyFileVersion("9.01.0.1590")]
 [assembly: NeutralResourcesLanguage("en-us")]
