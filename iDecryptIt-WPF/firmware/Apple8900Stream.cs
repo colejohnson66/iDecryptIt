f7ead4d2db25987d758b58cb356e9e44e1fc7781
Cole Tobin <coleharrisjohnson@gmail.com>
2016-04-14 00:29:53 +0000

    Replace Apple8900File with a proper Stream based implementation in Apple8900Stream

---
+++ iDecryptIt-WPF/firmware/Apple8900Stream.cs
@@ -0,0 +1,209 @@
+﻿/* =============================================================================
+ * File:   Apple8900Reader.cs
+ * Author: Cole Johnson
+ * =============================================================================
+ * Copyright (c) 2016 Cole Johnson
+ * 
+ * This file is part of iDecryptIt.
+ * 
+ * iDecryptIt is free software: you can redistribute it and/or modify it under
+ *   the terms of the GNU General Public License as published by the Free
+ *   Software Foundation, either version 3 of the License, or (at your option)
+ *   any later version.
+ * 
+ * iDecryptIt is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *   FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ *   more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ *   iDecryptIt. If not, see <http://www.gnu.org/licenses/>.
+ * =============================================================================
+ */
+using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Hexware.Programs.iDecryptIt.Firmware
+{
+    class Apple8900Stream : Stream, IDisposable
+    {
+        /* Apple8900 {
+         *    0  byte[4]  magic;   // "8900"
+         *    4  byte[3]  version; // "1.0"
+         *    7  byte     format;  // unencrypted == 0x4; encrypted == 0x3
+         *    8  uint32;
+         *    C  uint32   dataSize;
+         *   10  uint32   footerSigOffset;  // ignoring header
+         *   14  uint32   footerCertOffset; // ignoring header
+         *   18  uint32   footerCertSize;
+         *   1C  byte[32] salt;
+         *   3C  uint16;
+         *   3E  uint16   epoch;
+         *   40  byte[16] headerSig; // aes128cbc(sha1(file[0:0x40])[0:0x10], Key0x837, ZeroIV)
+         *   50  byte[0x7B0] padding;
+         *  800  Img2File payload;   // sizeof(payload) == dataSize
+         * ????  byte[?]  footer;    // padding, signature and certificate
+         */
+        
+        // <https://www.theiphonewiki.com/wiki/AES_Keys#Key_0x837>
+        // Used to encrypt/decrypt the contents of an 8900 file
+        private static readonly byte[] Key0x837 = new byte[]
+        {
+            0x18, 0x84, 0x58, 0xA6, 0xD1, 0x50, 0x34, 0xDF,
+            0xE3, 0x86, 0xF2, 0x3B, 0x61, 0xD4, 0x37, 0x74
+        };
+        private static readonly byte[] Magic = new byte[] { 0x38, 0x39, 0x30, 0x30 };
+        private static readonly byte[] Version = new byte[] { 0x31, 0x2E, 0x30 };
+
+        private Stream _stream;
+        private bool _encrypted = false;
+        private int _dataSize;
+        private byte[] _payload;
+        private int _seekPos;
+
+        public Apple8900Stream(Stream stream)
+        {
+            if (stream == null)
+                throw new ArgumentNullException("stream");
+            if (!stream.CanRead)
+                throw new ArgumentException("Stream must support reading.", "stream");
+            if (!stream.CanSeek)
+                throw new ArgumentException("Stream must support seeking.", "stream");
+
+            _stream = stream;
+        }
+
+        private void ParseStream()
+        {
+            _stream.Seek(0, SeekOrigin.Begin);
+
+            byte[] buf = new byte[0x800];
+            if (_stream.Read(buf, 0, 0x800) != 0x800)
+                throw new FileFormatException("Stream too small to contain an 8900 file.");
+
+            if (buf[0] != Magic[0] || buf[1] != Magic[1] ||
+                buf[2] != Magic[2] || buf[3] != Magic[3])
+                throw new FileFormatException("Stream is not an Apple 8900 file.");
+
+            if (buf[4] != '1' || buf[5] != '.' || buf[6] != '0')
+                throw new FileFormatException("Unknown Apple 8900 file version. Only 1.0 is supported.");
+
+            if (buf[7] != 3 && buf[7] != 4)
+                throw new FileFormatException("Unknown Apple 8900 file type. Only types 3 and 4 are supported.");
+            _encrypted = (buf[7] == 3);
+
+            _dataSize = BitConverter.ToInt32(buf, 0xC);
+            if (_dataSize <= 0)
+                throw new FileFormatException("Payload cannot have a zero or negative size.");
+
+            _payload = new byte[_dataSize];
+            _seekPos = 0;
+            if (_stream.Read(_payload, 0, _dataSize) != _dataSize)
+                throw new FileFormatException("Stream to small to contain indicated payload.");
+
+            if (_encrypted)
+                DecryptPayload();
+        }
+        private void DecryptPayload()
+        {
+            using (AesManaged aes = new AesManaged())
+            {
+                aes.BlockSize = 128;
+                aes.Mode = CipherMode.CBC;
+                aes.IV = new byte[16];
+                aes.Key = Key0x837;
+                try
+                {
+                    MemoryStream ms = new MemoryStream();
+                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        // write the encrypted data to the crypto stream
+                        cs.Write(_payload, 0, _payload.Length);
+                        // then read back the decrypted data and discard the encrypted version
+                        byte[] buf = ms.ToArray();
+                        _payload = buf;
+                    }
+                }
+                catch (CryptographicException)
+                {
+                    // ignore padding issue
+                }
+            }
+        }
+
+        public override bool CanRead
+        {
+            get
+            {
+                return _stream.CanRead;
+            }
+        }
+        public override bool CanSeek
+        {
+            get
+            {
+                return _stream.CanSeek;
+            }
+        }
+        public override bool CanWrite
+        {
+            get
+            {
+                // FIXME: Implement writing
+                return false;
+            }
+        }
+        public override long Length
+        {
+            get
+            {
+                return _dataSize;
+            }
+        }
+        public override long Position
+        {
+            get
+            {
+                return _seekPos;
+            }
+
+            set
+            {
+                Seek(value, SeekOrigin.Begin);
+            }
+        }
+
+        public override void Flush()
+        {
+            _stream.Flush();
+        }
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            // if `count' would put us past the end of the stream, fix it
+            if (_seekPos + count > _dataSize)
+                count = _dataSize - _seekPos;
+
+            Array.Copy(_payload, _seekPos, buffer, offset, count);
+            return count;
+        }
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            if (origin == SeekOrigin.Begin)
+                _seekPos = (int)offset;
+            else if (origin == SeekOrigin.Current)
+                _seekPos += (int)offset;
+            else
+                _seekPos = _dataSize - (int)offset;
+            return _seekPos;
+        }
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
