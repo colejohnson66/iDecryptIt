02b8aa5ecfb389a44720c7410d943744c58a195f
colejohnson66 <colejohnson66@209cdc03-6d9c-410e-8592-bf0908a4dd94>
2016-04-14 00:29:53 +0000

    Replace Apple8900File with a proper Stream based implementation in Apple8900Stream

--- trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ trunk/iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("9.31.0.1661")]
-[assembly: AssemblyFileVersion("9.31.0.1661")]
+[assembly: AssemblyVersion("9.31.0.1664")]
+[assembly: AssemblyFileVersion("9.31.0.1664")]
 [assembly: NeutralResourcesLanguage("en-us")]
