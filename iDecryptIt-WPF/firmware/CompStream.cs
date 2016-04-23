077b7a2b735b59b57a0c8ead2aae07498b2e4703
Cole Tobin <coleharrisjohnson@gmail.com>
2016-04-23 21:44:29 +0000

    Implemented IMG2 files, Comp files, and iBoot images Fixed bug in 8900 reader

--- iDecryptIt-WPF/firmware/CompStream.cs
+++ iDecryptIt-WPF/firmware/CompStream.cs
@@ -1,5 +1,5 @@
 ﻿/* =============================================================================
- * File:   Apple8900Stream.cs
+ * File:   CompStream.cs
  * Author: Cole Johnson
  * =============================================================================
  * Copyright (c) 2016 Cole Johnson
@@ -22,52 +22,34 @@
  */
 using System;
 using System.IO;
-using System.Security.Cryptography;
 
 namespace Hexware.Programs.iDecryptIt.Firmware
 {
-    // This stream exposes the payload of the file as a stream, NOT the 8900 wrapper
-    // A better name would be Apple8900PayloadStream, but in the future, this class
-    // will allow changing the footer and other data.
-    public class Apple8900Stream : Stream
+    public class CompStream : Stream
     {
-        /* Apple8900 {
-         *    0  byte[4]  magic;   // "8900"
-         *    4  byte[3]  version; // "1.0"
-         *    7  byte     format;  // unencrypted == 0x4; encrypted == 0x3; types 0x1 and 0x2 exist, but are unsupported
-         *    8  uint32;
-         *    C  uint32   payloadLength;
-         *   10  uint32   footerSigOffset;  // ignoring header
-         *   14  uint32   footerCertOffset; // ignoring header
-         *   18  uint32   footerCertSize;
-         *   1C  byte[32] salt;
-         *   3C  uint16;
-         *   3E  uint16   epoch;
-         *   40  byte[16] headerSig; // aes128cbc(sha1(file[0:0x40])[0:0x10], Key0x837, ZeroIV)
-         *   50  byte[0x7B0] padding;
-         *  800  byte[]   payload;   // sizeof(payload) == payloadLength (IMG2, DMG, or raw data)
-         * ????  byte[?]  footer;    // padding, signature and certificate
+        /* Comp {
+         *    0  byte[4]  magic;     // "comp"
+         *    4  uint32   compressionType;
+         *    8  uint32   checksum;
+         *    C  uint32   decompressedLength;   // big endian
+         *   10  uint32   compressedLength; // big endian
+         *   14  byte[0x16C] padding;
+         *  180  byte[]   payload;   // sizeof(payload) == compressedLength
          * }
          */
-        
-        // <https://www.theiphonewiki.com/wiki/AES_Keys#Key_0x837>
-        // Used to encrypt/decrypt the contents of an 8900 file
-        private static readonly byte[] Key0x837 = new byte[]
-        {
-            0x18, 0x84, 0x58, 0xA6, 0xD1, 0x50, 0x34, 0xDF,
-            0xE3, 0x86, 0xF2, 0x3B, 0x61, 0xD4, 0x37, 0x74
-        };
-        private static readonly uint Magic = 0x30303938u; // "8900" in big endian
+
+        private static readonly uint Magic = 0x706D6F63; // "comp" in big endian
+        private static readonly uint LzssSignature = 0x73737A6C; // "lzss" in big endian
 
         private Stream _stream;
         private byte[] _payload;
         private int _seekPos;
 
-        public Apple8900Stream(Stream stream)
+        public CompStream(Stream stream)
             : this(stream, true)
         { }
 
-        public Apple8900Stream(Stream stream, bool resetStreamPosition)
+        public CompStream(Stream stream, bool resetStreamPosition)
         {
             if (stream == null)
                 throw new ArgumentNullException("stream");
@@ -85,62 +67,35 @@ namespace Hexware.Programs.iDecryptIt.Firmware
             if (resetPos)
                 _stream.Seek(0, SeekOrigin.Begin);
 
-            byte[] buf = new byte[0x800];
-            if (_stream.Read(buf, 0, 0x800) != 0x800)
-                throw new FileFormatException("Stream too small to contain an Apple 8900 file.");
+            byte[] buf = new byte[0x180];
+            if (_stream.Read(buf, 0, 0x180) != 0x180)
+                throw new FileFormatException("Stream too small to contain a Comp file.");
 
             if (BitConverter.ToUInt32(buf, 0) != Magic)
-                throw new FileFormatException("Stream is not an Apple 8900 file.");
+                throw new FileFormatException("Stream is not a Comp file.");
 
-            if (buf[4] != '1' || buf[5] != '.' || buf[6] != '0')
-                throw new FileFormatException("Unknown Apple 8900 file version. Only 1.0 is supported.");
+            if (BitConverter.ToUInt32(buf, 4) != LzssSignature)
+                throw new FileFormatException("Unknown Comp compression type. Only LZSS is supported.");
 
-            if (buf[7] != 3 && buf[7] != 4)
-                throw new FileFormatException("Unknown Apple 8900 file type. Only types 3 and 4 are supported.");
-
-            bool encrypted = (buf[7] == 3);
-
-            int payloadLength = BitConverter.ToInt32(buf, 0xC);
-            if (payloadLength <= 0)
+            // the lengths are stored in big endian; reverse them
+            byte[] tmp = new byte[4];
+            Array.Copy(buf, 0xC, tmp, 0, 4);
+            Array.Reverse(tmp);
+            int decompLen = BitConverter.ToInt32(tmp, 0);
+            Array.Copy(buf, 0x10, tmp, 0, 4);
+            Array.Reverse(tmp);
+            int compLen = BitConverter.ToInt32(tmp, 0);
+            if (compLen <= 0 || decompLen <= 0)
                 throw new FileFormatException("Payload cannot have a zero or negative size.");
 
-            _payload = new byte[payloadLength];
-            _seekPos = 0;
-            if (_stream.Read(_payload, 0, payloadLength) != payloadLength)
+            byte[] compPayload = new byte[compLen];
+            _payload = new byte[decompLen];
+            if (_stream.Read(compPayload, 0, compLen) != compLen)
                 throw new FileFormatException("Stream to small to contain indicated payload.");
 
-            if (encrypted)
-                DecryptPayload();
-
-            // TODO: Read signiature and certificate
-            // To read sig, read 0x80 bytes from `0x800+footerSigOffset'
-            // To read cert, read `footerCertSize' bytes from `0x800+footerCertOffset'
-        }
-        private void DecryptPayload()
-        {
-            using (AesManaged aes = new AesManaged())
-            {
-                aes.BlockSize = 128;
-                aes.Mode = CipherMode.CBC;
-                aes.IV = new byte[16];
-                aes.Key = Key0x837;
-                try
-                {
-                    MemoryStream ms = new MemoryStream();
-                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
-                    {
-                        // write the encrypted data to the crypto stream
-                        cs.Write(_payload, 0, _payload.Length);
-                        // then read back the decrypted data and discard the encrypted version
-                        byte[] buf = ms.ToArray();
-                        _payload = buf;
-                    }
-                }
-                catch (CryptographicException)
-                {
-                    // ignore padding issue
-                }
-            }
+            int length = Lzss.Decompress(_payload, compPayload);
+            if (length != decompLen)
+                throw new FileFormatException("Decompressed stream not expected size.");
         }
 
         public override bool CanRead
