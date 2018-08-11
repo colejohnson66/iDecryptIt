e7906089e34bc1bab6ff4fcfc259116a46af7f16
Cole Tobin <coleharrisjohnson@gmail.com>
2018-08-11 16:47:27 +0000

    12.00 -> 11.41

--- iDecryptIt-WPF/Properties/AssemblyInfo.cs
+++ iDecryptIt-WPF/Properties/AssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(true)]
 [assembly: Guid("ba12c942-94e7-4c25-b4b9-29c3e37dbf14")]
 
-[assembly: AssemblyVersion("12.00.0.1735")]
-[assembly: AssemblyFileVersion("12.00.0.1735")]
+[assembly: AssemblyVersion("11.41.0.1735")]
+[assembly: AssemblyFileVersion("11.41.0.1735")]
 [assembly: NeutralResourcesLanguage("en-us")]
