61c7813e9c80576cbc32e783730b422ead9c3008
Cole Tobin <coleharrisjohnson@gmail.com>
2018-08-14 18:22:54 +0000

    Beginning work on programically adding keys to the get keys pane

--- iDecryptIt-WPF/MainWindow2.xaml.cs
+++ iDecryptIt-WPF/MainWindow2.xaml.cs
@@ -73,17 +73,24 @@ namespace Hexware.Programs.iDecryptIt
             GetKeysDeviceComboBox.ItemsSource = KeySelectionLists.Products;
 
             this.Dispatcher.UnhandledException += Dispatcher_UnhandledException;
-
-            //InitKeyGridDictionary();
         }
 
         private void Dispatcher_UnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
         {
-            Trace(nameof(Dispatcher_UnhandledException), sender, e);
+            try
+            {
+                Trace(nameof(Dispatcher_UnhandledException), sender, e);
+            }
+            catch (Exception ex)
+            { }
 
             FatalError("An unknown error has occured.", e.Exception);
             Close();
             e.Handled = true;
+        }
+        private void Error(string v, Exception ex)
+        {
+            throw new NotImplementedException();
         }
         private void FatalError(string v, Exception exception)
         {
@@ -100,7 +107,14 @@ namespace Hexware.Programs.iDecryptIt
         {
             if (!Globals.Trace)
                 return;
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("{0}{1}()", "[TRACE]".PadRight(12), functionName);
+                return;
+            }
             Console.WriteLine("{0}{1}(", "[TRACE]".PadRight(12), functionName);
+            
             for (int i = 0; i < args.Length; i++)
             {
                 Console.Write("                {0}", args[i]); // 16 spaces for a 4 space indent
@@ -242,6 +256,252 @@ namespace Hexware.Programs.iDecryptIt
 
             ComboBoxEntry entry = (ComboBoxEntry)e.AddedItems[0];
             selectedVersion = entry.ID;
+
+            Stream stream = GetKeyStream(selectedModel + "_" + selectedVersion + ".plist");
+            if (stream == Stream.Null)
+            {
+                Debug("[KEYSELECT]", "Key file doesn't exist. No keys available.");
+                //MessageBox.Show(
+                //    "Sorry, but that version doesn't have any published keys.",
+                //    "iDecryptIt",
+                //    MessageBoxButton.OK,
+                //    MessageBoxImage.Information);
+                return;
+            }
+            LoadKeys(stream);
+        }
+
+        private void LoadKeys(Stream document)
+        {
+            Trace(nameof(LoadKeys), document);
+            
+            GetKeysClearStackPanel();
+            GetKeysAddDeviceAndVersion("Device", "Version", "Build");
+            GetKeysAddRootFS();
+            GetKeysAddEncryptedItem("Restore Ramdisk", "RestoreRamdisk");
+            GetKeysAddNotEncryptedItem("Update Ramdisk", "UpdateRamdisk");
+        }
+        internal Stream GetKeyStream(string fileName)
+        {
+            Trace(nameof(GetKeyStream), fileName);
+
+            Debug("[GETSTREAM]", "Attempting read of stored resource, \"" + fileName + "\".");
+
+            if (!Globals.KeyArchive.FileExists(fileName))
+                return Stream.Null;
+
+            try
+            {
+                return Globals.KeyArchive.GetFile(fileName);
+            }
+            catch (Exception ex)
+            {
+                Error("Unable to retrieve keys.", ex);
+            }
+            return Stream.Null;
+        }
+
+        private void GetKeysClearStackPanel()
+        {
+            Trace(nameof(GetKeysClearStackPanel));
+
+            GetKeysStackPanel.Children.Clear();
+        }
+        private void GetKeysAddDeviceAndVersion(string device, string version, string build)
+        {
+            Trace(nameof(GetKeysAddDeviceAndVersion), device, version, build);
+
+            //<TextBlock Text="Device" x:Name="txtDevice" FontSize="24" />
+            //<TextBlock Text="Version (Build)" x:Name="txtVersion" FontSize="16" Margin="12,6,0,6" />
+            TextBlock deviceBlock = new TextBlock();
+            deviceBlock.Text = device;
+            deviceBlock.FontSize = 24;
+            GetKeysStackPanel.Children.Add(deviceBlock);
+
+            TextBlock versionBlock = new TextBlock();
+            versionBlock.Text = String.Format("{0} ({1})", version, build);
+            versionBlock.FontSize = 16;
+            versionBlock.Margin = new Thickness(12, 6, 0, 6);
+            GetKeysStackPanel.Children.Add(versionBlock);
+        }
+        private void GetKeysAddRootFS()
+        {
+            Trace(nameof(GetKeysAddRootFS));
+
+            Grid grid = new Grid();
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(125, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(6, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(6, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(125, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(6, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(75, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(6, GridUnitType.Pixel) });
+            grid.Margin = new Thickness(0, 3, 0, 3);
+
+            TextBlock rootFSKeyLabel = new TextBlock();
+            rootFSKeyLabel.Text = "Root FS Key";
+            rootFSKeyLabel.VerticalAlignment = VerticalAlignment.Center;
+            grid.Children.Add(rootFSKeyLabel);
+
+            TextBox key = new TextBox();
+            key.Name = "RootFSKey";
+            key.IsReadOnly = true;
+            key.SetValue(Grid.ColumnProperty, 2);
+            grid.Children.Add(key);
+
+            TextBox filename = new TextBox();
+            filename.Name = "RootFSFilename";
+            filename.IsReadOnly = true;
+            filename.SetValue(Grid.ColumnProperty, 4);
+            grid.Children.Add(filename);
+
+            Button decrypt = new Button();
+            decrypt.Name = "RootFSDecrypt";
+            decrypt.Content = "Decrypt >";
+            decrypt.SetValue(Grid.ColumnProperty, 6);
+            decrypt.Click += GetKeysDecryptButton_Click;
+            grid.Children.Add(decrypt);
+
+            GetKeysStackPanel.Children.Add(grid);
+        }
+        private void GetKeysAddNotEncryptedItem(string firmwareItem, string firmwareKey)
+        {
+            Trace(nameof(GetKeysAddNotEncryptedItem), firmwareItem, firmwareKey);
+
+            Grid grid = new Grid();
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(125, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(6, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(6, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(125, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(6, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(75, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(6, GridUnitType.Pixel) });
+            grid.Margin = new Thickness(0, 3, 0, 3);
+
+            TextBlock label = new TextBlock();
+            label.Text = firmwareItem;
+            label.VerticalAlignment = VerticalAlignment.Center;
+            grid.Children.Add(label);
+
+            TextBlock key = new TextBlock();
+            key.Name = firmwareKey + "NotEncrypted";
+            key.Text = "Not Encrypted";
+            key.VerticalAlignment = VerticalAlignment.Center;
+            key.HorizontalAlignment = HorizontalAlignment.Center;
+            key.SetValue(Grid.ColumnProperty, 2);
+            grid.Children.Add(key);
+
+            TextBox filename = new TextBox();
+            filename.Name = firmwareKey + "Filename";
+            filename.IsReadOnly = true;
+            filename.SetValue(Grid.ColumnProperty, 4);
+            grid.Children.Add(filename);
+
+            Button decrypt = new Button();
+            decrypt.Name = firmwareKey + "Decrypt";
+            decrypt.Content = "Decrypt >";
+            decrypt.SetValue(Grid.ColumnProperty, 6);
+            decrypt.Click += GetKeysDecryptButton_Click;
+            grid.Children.Add(decrypt);
+
+            GetKeysStackPanel.Children.Add(grid);
+        }
+        private void GetKeysAddEncryptedItem(string firmwareItem, string firmwareKey)
+        {
+            Trace(nameof(GetKeysAddEncryptedItem), firmwareItem, firmwareKey);
+
+            Grid grid = new Grid();
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(125, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(6, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(6, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(125, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(6, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(75, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(6, GridUnitType.Pixel) });
+            grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Auto) });
+            grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(6, GridUnitType.Pixel) });
+            grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Auto) });
+            grid.Margin = new Thickness(0, 3, 0, 3);
+
+            TextBlock ivLabel = new TextBlock();
+            ivLabel.Text = firmwareItem + " (IV)";
+            ivLabel.VerticalAlignment = VerticalAlignment.Center;
+            grid.Children.Add(ivLabel);
+
+            TextBlock keyLabel = new TextBlock();
+            keyLabel.Text = firmwareItem + " (Key)";
+            keyLabel.VerticalAlignment = VerticalAlignment.Center;
+            keyLabel.SetValue(Grid.RowProperty, 2);
+            grid.Children.Add(keyLabel);
+
+            TextBox iv = new TextBox();
+            iv.Name = firmwareKey + "IV";
+            iv.IsReadOnly = true;
+            iv.SetValue(Grid.ColumnProperty, 2);
+            grid.Children.Add(iv);
+
+            TextBox key = new TextBox();
+            key.Name = firmwareKey + "Key";
+            key.IsReadOnly = true;
+            key.SetValue(Grid.ColumnProperty, 2);
+            key.SetValue(Grid.RowProperty, 2);
+            grid.Children.Add(key);
+
+            TextBox filename = new TextBox();
+            filename.Name = firmwareKey + "Filename";
+            filename.IsReadOnly = true;
+            filename.SetValue(Grid.ColumnProperty, 4);
+            filename.SetValue(Grid.RowSpanProperty, 3);
+            filename.VerticalAlignment = VerticalAlignment.Center;
+            grid.Children.Add(filename);
+
+            Button decrypt = new Button();
+            decrypt.Name = firmwareKey + "Decrypt";
+            decrypt.Content = "Decrypt >";
+            decrypt.SetValue(Grid.ColumnProperty, 6);
+            decrypt.SetValue(Grid.RowSpanProperty, 3);
+            decrypt.VerticalAlignment = VerticalAlignment.Center;
+            decrypt.Click += GetKeysDecryptButton_Click;
+            grid.Children.Add(decrypt);
+
+            GetKeysStackPanel.Children.Add(grid);
+        }
+
+        private void GetKeysDecryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            Trace(nameof(GetKeysDecryptButton_Click), sender, e);
+
+            // ((Button)sender).Name contains the button name
+
+            object obj = GetKeysStackPanelFindObject("RootFSKey");
+        }
+        private UIElement GetKeysStackPanelFindObject(string name)
+        {
+            Trace(nameof(GetKeysStackPanelFindObject), name);
+
+            foreach (UIElement possibleGrid in GetKeysStackPanel.Children)
+            {
+                if (possibleGrid.GetType() == typeof(Grid))
+                {
+                    foreach (UIElement gridItem in ((Grid)possibleGrid).Children)
+                    {
+                        if (gridItem.GetType() == typeof(TextBlock) &&
+                            ((TextBlock)gridItem).Name == name)
+                            return gridItem;
+                        if (gridItem.GetType() == typeof(TextBox) &&
+                            ((TextBox)gridItem).Name == name)
+                            return gridItem;
+                        if (gridItem.GetType() == typeof(Button) &&
+                            ((Button)gridItem).Name == name)
+                            return gridItem;
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
