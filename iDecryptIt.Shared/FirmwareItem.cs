4bfb07e10039fab073a86b9a8d020a1f5d090e40
Cole Johnson <coleharrisjohnson@gmail.com>
2022-03-11 22:06:56 -0500

    Add copyright header to all files from this rewrite

--- iDecryptIt.Shared/FirmwareItem.cs
+++ iDecryptIt.Shared/FirmwareItem.cs
@@ -1,4 +1,27 @@
-﻿using JetBrains.Annotations;
+﻿/* =============================================================================
+ * File:   FirmwareItem.cs
+ * Author: Cole Tobin
+ * =============================================================================
+ * Copyright (c) 2022 Cole Tobin
+ *
+ * This file is part of iDecryptIt.
+ *
+ * iDecryptIt is free software: you can redistribute it and/or modify it under
+ *   the terms of the GNU General Public License as published by the Free
+ *   Software Foundation, either version 3 of the License, or (at your option)
+ *   any later version.
+ *
+ * iDecryptIt is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *   FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ *   more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ *   iDecryptIt. If not, see <http://www.gnu.org/licenses/>.
+ * =============================================================================
+ */
+
+using JetBrains.Annotations;
 using System.Diagnostics;
 using System.Diagnostics.Contracts;
 using System.IO;
