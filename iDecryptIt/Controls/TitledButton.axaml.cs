aedb721daeaeca8e4e443ba802c76b6c46473f2b
Cole Tobin <coleharrisjohnson@gmail.com>
2022-08-07 00:04:40 -0400

    Being rework to use AvaloniaSpacedGrid (plus some styling cleanup)

---
+++ iDecryptIt/Controls/TitledButton.axaml.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+using System.Windows.Input;
+
+namespace iDecryptIt.Controls;
+
+public class TitledButton : TemplatedControl
+{
+    public static readonly StyledProperty<ICommand> CommandProperty =
+        AvaloniaProperty.Register<TitledButton, ICommand>(nameof(Command));
+    public ICommand Command
+    {
+        get => GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
+    public static readonly StyledProperty<IImage> ImageProperty =
+        AvaloniaProperty.Register<TitledButton, IImage>(nameof(Image));
+    public IImage Image
+    {
+        get => GetValue(ImageProperty);
+        set => SetValue(ImageProperty, value);
+    }
+
+    public static readonly StyledProperty<string> TextProperty =
+        AvaloniaProperty.Register<TitledButton, string>(nameof(Text));
+    public string Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+}
